788488f45e476f88fca94f05208b2ae6672c7dd7
ilyashaaa <86850598+ilyashaa@users.noreply.github.com>
2023-07-23 18:13:31 +0300

    Обновление entity
    Удаление ненужных переменных

--- lib/entity/image_start.dart
+++ lib/entity/image_start.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ImageStart {
-  final int id;
   final String title;
   final String way;
 
-  ImageStart(this.id, this.title, this.way);
+  ImageStart(this.title, this.way);
 }
 
 List<ImageStart> imageStartList = [
-  ImageStart(1, '25 августа 2023', 'assets/imageTop/first.png'),
-  ImageStart(2, 'Будет праздник!', 'assets/imageTop/second.jpg'),
+  ImageStart('25 августа 2023', 'assets/imageTop/first.png'),
+  ImageStart('Будет праздник!', 'assets/imageTop/second.jpg'),
   ImageStart(
-      3, 'С собой иметь настроение и деньги!', 'assets/imageTop/third.jpeg'),
-  ImageStart(4, 'Coming soon..', 'assets/imageTop/fourth.jpg'),
+      'С собой иметь настроение и деньги!', 'assets/imageTop/third.jpeg'),
+  ImageStart('Coming soon..', 'assets/imageTop/fourth.jpg'),
 ];
--- lib/entity/menu_list.dart
+++ lib/entity/menu_list.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MenuList {
-  final int id;
   final String foodName;
   final String way;
 
-  MenuList(this.id, this.foodName, this.way);
+  MenuList(this.foodName, this.way);
 }
 
 List<MenuList> menuList = [
-  MenuList(1, 'Канапе', 'assets/imageMenu/1.jpg'),
-  MenuList(2, 'Сырная тарелка', 'assets/imageMenu/2.png'),
-  MenuList(3, 'Шашлык на мангале', 'assets/imageMenu/3.jpg'),
-  MenuList(4, 'Морепродукты', 'assets/imageMenu/4.webp'),
-  MenuList(5, 'Свежие фрукты', 'assets/imageMenu/5.jpg'),
-  MenuList(6, 'Авторские лемонады', 'assets/imageMenu/6.jpg'),
+  MenuList('Канапе', 'assets/imageMenu/1.jpg'),
+  MenuList('Сырная тарелка', 'assets/imageMenu/2.png'),
+  MenuList('Шашлык на мангале', 'assets/imageMenu/3.jpg'),
+  MenuList('Морепродукты', 'assets/imageMenu/4.webp'),
+  MenuList('Свежие фрукты', 'assets/imageMenu/5.jpg'),
+  MenuList('Авторские лемонады', 'assets/imageMenu/6.jpg'),
 ];
